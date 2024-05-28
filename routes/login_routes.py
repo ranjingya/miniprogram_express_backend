e5e2662b958d4992fee58ee493736ef632c685b6
ranjingya <109840550+ranjingya@users.noreply.github.com>
2024-05-28 22:26:57 +0800

    Update login_routes.py
    修改appid为空

--- routes/login_routes.py
+++ routes/login_routes.py
@@ -8,34 +8,6 @@ def login():
     # （已注册）用户登录，先校验redis中的token，有就放行，否则获取用户名密码与数据库校验，成功生成token存入redis
     # （未注册）用户登录，获取openId，注册信息存入数据库
 
-    # user_info = {}
-    #
-    # if 'username' and 'password' in request.json:
-    #     user_info = {
-    #         'username': request.json.get('username'),
-    #         'password': request.json.get('password'),
-    #     }
-    #     print(user_info)
-    #
-    # # 未登录，已注册，校验用户名密码，删除已存在的token，生成新token存入redis
-    # is_exist_username = UserUtil.find_by_username(user_info)
-    # if is_exist_username:
-    #     print('用户登录：登录用户名密码正确，生成token：' + is_exist_username)
-    #     return jsonify({
-    #         'code': 200,
-    #         'msg': '登录成功',
-    #         'data': {
-    #             'Authorization': is_exist_username
-    #         }
-    #     }), 200
-    # elif is_exist_username == False:
-    #     return jsonify({
-    #         'code': 403,
-    #         'msg': '登录失败，用户名密码错误'
-    #     }), 403
-    #
-    # else:
-
     # 已登录
     if 'Authorization' in request.headers:
         is_exist_redis_openid = UserUtil.exist_redis_open_id(request.headers.get('Authorization'))
@@ -50,8 +22,8 @@ def login():
                 'msg': 'token不存在，请登录'
             }), 401
 
-    app_id = 'wxc419699573b7e97f'
-    secret = 'bfc17df99f502f1116baa5b7f614c3f5'
+    app_id = ''    # 你自己的app_id
+    secret = ''    # 你自己的开发者app secret
     js_code = request.json.get('code')
     print('js_code:' + js_code)
     query = f'?appid={app_id}&secret={secret}&js_code={js_code}&grant_type=authorization_code'
